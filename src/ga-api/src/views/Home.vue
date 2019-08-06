aee20490a9ea8925987649c655abf0d14c244589
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2019-08-06 10:05:00 +0900

    add some binding
    https://github.com/vuejs-jp/hol-vue-ts-basic/blob/master/instructions/lab03.md
    そのままですが

--- src/ga-api/src/views/Home.vue
+++ src/ga-api/src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h3>テキストバインディング</h3>
+    <span>{{ msg }}</span>
+    <h3>リストレンダリング</h3>
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{item.name}}
+      </li>
+    </ul>
+    <h3>イベントハンドリング</h3>
+<button v-on:click="onClick">実行</button>
   </div>
 </template>
 
+
+<style scoped>
+.home {
+  text-align: left;
+}
+</style>
+
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+
+// ここにインターフェースを追加
+declare interface Item {
+  id: number;
+  name: string;
+}
 
 export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      msg: "Hello Vue.js!",
+      items: [
+        {
+          id: 1,
+          name: "キーボード"
+        },
+        {
+          id: 2,
+          name: "マウス"
+        }
+      ] as Item[]
+    };
   },
+  // ここにメソッドを追加する
+methods: {
+  onClick(): void {
+    alert(this.msg);
+  }
+}
 });
 </script>
